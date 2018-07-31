cbe8869de37f1eaa049725473e423e12928572b5
Thorsten Zoerner <me@thorsten-zoerner.com>
2018-07-31 14:52:24 +0200

    Added Wallet

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,66 @@
+function unlockBrain() {
+  $('#login_frm').hide();
+    document.app.provider=ethers.providers.getDefaultProvider("homestead");
+    ethers.Wallet.fromBrainWallet($('#username').val(),$('#password').val()).then(function(wallet) {
+        $('#wallet_address').val(wallet.address);
+        console.log(wallet.address);
+        document.app.getInfo();
+    });
+}
+
 $(document).ready(function() {
   document.app= {};
 
-  var getInfo=function() {
+  document.app.getInfo=function() {
+    var retrieveBalance=function() {
+      contract.balanceOf($('#wallet_address').val()).then(function(b) {
+          $('#cori_balance').html((b.toString()/100).toFixed(2));
+      });
+      exd.balanceOf($('#wallet_address').val()).then(function(b) {
+          $('#cori_dividend').html((ethers.utils.formatEther(b)*1).toFixed(7));
+      });
+      document.app.provider.getBalance($('#wallet_address').val()).then(function(balance) {
+            $('#eth_balance').html((ethers.utils.formatEther(balance)*1).toFixed(7));
+      });
+    }
+
       var e20abi=[{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"value","type":"uint256"},{"name":"extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}];
+      var exDabi=[{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalDividend","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"withdrawDividend","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"_account","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"acceptOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"newOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalClaimed","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"tokenAddress","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferAnyERC20Token","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"divMultiplier","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"value","type":"uint256"}],"name":"withdrawBonds","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"token","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_token","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_value","type":"uint256"}],"name":"Dividend","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"account","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Payed","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_value","type":"uint256"}],"name":"Withdraw","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"}],"name":"OwnershipTransferred","type":"event"}];
     var contract = new ethers.Contract("0x725b190bc077ffde17cf549aa8ba25e298550b18", e20abi,ethers.providers.getDefaultProvider("homestead"));
+    var exd = new ethers.Contract("0xa050b90B0C60900E304774Eb39220dFB2C5fFf0F", exDabi,ethers.providers.getDefaultProvider("homestead"));
     contract.totalSupply().then(function(ts) {
-        $('#totalSupply').html((ts.toString()/100).toFixed(2));
+        $('.totalSupply').html((ts.toString()/100).toFixed(2));
     });
-    document.app.provider=ethers.providers.getDefaultProvider("homestead");
-  }
+    if(typeof document.app.provider == "undefined") {
+      document.app.provider=ethers.providers.getDefaultProvider("homestead");
+    }
+
+    $('#retrieveBalance').click(retrieveBalance);
+    if((typeof web3 != "undefined") && (typeof web3.eth != "undefined")) {
+        if($('#wallet_address').val().length<42) {
+          $('#wallet_address').val(web3.eth.accounts[0]);
+          retrieveBalance();
+        }
+    }
+    if($('#wallet_address').val().length==42) {
+      retrieveBalance();
+    }
+}
 
 
 
 if (typeof web3 !== 'undefined') {
+      $('#login_frm').hide();
       var web3Provider = new ethers.providers.Web3Provider(web3.currentProvider,ethers.providers.getDefaultProvider("homestead"));
       web3Provider.getBalance("0xc430fAB09288C272A321C086d330609CD8b71447"). then(function(balance) {
+        document.app.provider=web3Provider;
         var etherString = ethers.utils.formatEther(balance);
-        getInfo();
+        document.app.getInfo();
       });
 } else {
+    document.app.provider=ethers.providers.getDefaultProvider("homestead");
+    $('#login_frm').show();
 
-
+    $('#unlockUsername').click(unlockBrain);
 }
 });
