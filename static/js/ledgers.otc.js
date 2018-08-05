6222301cb0f5640d7db40a7f76a582e313eae035
Thorsten Zoerner <me@thorsten-zoerner.com>
2018-08-05 02:49:39 +0200

    Sun build

--- static/js/ledgers.otc.js
+++ static/js/ledgers.otc.js
@@ -1,9 +1,13 @@
+
 $.qparam = function (name) {
   var results = new RegExp('[\?&]' + name + '=([^&#]*)')
                     .exec(window.location.search);
 
   return (results !== null) ? results[1] || 0 : false;
 }
+
+var e20abi=[{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"value","type":"uint256"},{"name":"extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}];
+
 
 function shortName(kyc,address) {
   if(typeof kyc[address]=="undefined") {
@@ -22,25 +26,35 @@ function app(kyc) {
   document.app.provider=ethers.providers.getDefaultProvider("homestead");
 
   var provider = ethers.providers.getDefaultProvider();
-
-  $('#account').html(shortName(kyc,$.qparam('a')));
+  var myname=shortName(kyc,$.qparam('a'));
+  $('#account').html(myname);
+  if(typeof kyc[$.qparam('a')].short_name!="undefined") {
+  document.title =  kyc[$.qparam('a')].short_name+" Accounting";
+  }
   if((typeof kyc[$.qparam('a')]!="undefined")&&(typeof kyc[$.qparam('a')].description!="undefined")) {
       $('#description').html(kyc[$.qparam('a')].description);
   }
   provider.getBalance($.qparam('a')).then(function(balance) {
     $('#balance').html((ethers.utils.formatEther(balance).toString()*1).toFixed(4));
     $('#balance').attr('data',ethers.utils.formatEther(balance).toString()*1);
+    var contract = new ethers.Contract("0x725b190bc077ffde17cf549aa8ba25e298550b18", e20abi,ethers.providers.getDefaultProvider("homestead"));
+    contract.balanceOf($.qparam('a')).then(function(cori_balance) {
+        $('#cori').html((cori_balance.toString()/100).toFixed(2));
+        $('#cori').attr('data',cori_balance.toString()*1);
+        getTransactionsClean();
+    });
   });
 
 
   function getTransactionsClean() {
     var startblock=6000000;
-    if(window.localStorage.getItem("txcache_"+$.qparam('a'))!=null) {
+    if((window.localStorage.getItem("txcache_"+$.qparam('a'))!=null)&&($.qparam('reset')==null)) {
       txcache=JSON.parse(window.localStorage.getItem("txcache_"+$.qparam('a')));
       startblock=txcache[txcache.length-1].blockNumber;
     } else {
       txcache=[];
     }
+    //startblock=6000000;
     $.getJSON("https://api.etherscan.io/api?module=account&action=txlist&address="+$.qparam('a')+"&startblock="+startblock+"&endblock=99999999&sort=asc&apikey=YourApiKeyToken",function(remoteCacheIt) {
       $.getJSON("https://api.etherscan.io/api?module=account&action=txlistinternal&address="+$.qparam('a')+"&startblock="+startblock+"&endblock=99999999&sort=asc&apikey=YourApiKeyToken",function(remoteCacheIn) {
       results=[];
@@ -48,14 +62,15 @@ function app(kyc) {
       // Merge the two lists into one
       var results_cache={};
       for(var i=0;i<remoteCacheIt.result.length;i++) {
-        results_cache[remoteCacheIt.result[i].blockNumber]=1;
+        results_cache[remoteCacheIt.result[i].blockNumber]=remoteCacheIt.result[i];
         results.push(remoteCacheIt.result[i]);
       }
       for(var i=0;i<remoteCacheIn.result.length;i++) {
         if(typeof results_cache[remoteCacheIn.result[i].blockNumber]=="undefined") {
             results.push(remoteCacheIn.result[i]);
         } else {
-            remoteCacheIn.result[i].blockNumber=remoteCacheIn.result[i]+"_internal";
+            console.log("compare",results_cache[remoteCacheIn.result[i].blockNumber],  remoteCacheIn.result[i]);
+            remoteCacheIn.result[i].blockNumber=remoteCacheIn.result[i].blockNumber+"_internal";
             results.push(remoteCacheIn.result[i]);
         }
       }
@@ -119,8 +134,7 @@ function app(kyc) {
 
                 }
                 if(cleaned[i].from==$.qparam("a")) {
-                  balance_entry.peer=cleaned[i].to;
-                  console.log(cleaned[i],cleaned[i].gasUsed*1*cleaned[i].gasPrice);
+                  balance_entry.peer=cleaned[i].to;                  
                   if(cleaned[i].type!="call") {
                     gas_used+=ethers.utils.formatEther(cleaned[i].gasUsed)*ethers.utils.formatEther(cleaned[i].gasPrice*1);
                   }
@@ -137,7 +151,6 @@ function app(kyc) {
                 }
               }
             }
-            console.log(gas_used);
             gasUsed=gas_used;
             var asset_rows=[];
             var asset_eths=gasUsed*1;
@@ -146,7 +159,9 @@ function app(kyc) {
                 var row="<td><a href='?a="+key+"'>"+balance_assets[key].peer_friendly+"</a></td><td align='right'>"+balance_assets[key].eur.toFixed(2)+"</td><td align='right'>"+balance_assets[key].eth.toFixed(4)+"</td>";
                 asset_eths+=balance_assets[key].eth;
                 asset_eurs+=balance_assets[key].eur;
-                asset_rows.push(row);
+                if(balance_assets[key].eth>0) {
+                  asset_rows.push(row);
+                }
             };
 
             var liability_rows=[];
@@ -159,24 +174,40 @@ function app(kyc) {
                 var row="<td><a href='?a="+key+"'>"+balance_liabilities[key].peer_friendly+"</a></td><td align='right'>"+balance_liabilities[key].eur.toFixed(2)+"</td><td align='right'>"+balance_liabilities[key].eth.toFixed(4)+"</td>";
                 liability_eths+=balance_liabilities[key].eth;
                 liability_eurs+=balance_liabilities[key].eur;
-                liability_rows.push(row);
+                if(balance_liabilities[key].eth>0) {
+                    liability_rows.push(row);
+                }
             };
-
 
             // Add Abgrenzungsposten
             var xrate_assets=asset_eurs/asset_eths;
             var xrate_liability=liability_eurs/liability_eths;
             asset_eurs+=gasUsed*xrate_assets;
+            var eth_price_cori=ethers.utils.formatEther(462313000000000);
+            var cori_val=$('#cori').attr('data')*eth_price_cori;
+            var cori_eur=$('#cori').attr('data')*eth_price_cori*xrate_liability;
+            liability_eths+=cori_val*1;
+            liability_eurs+=cori_eur*1;
+
+            liability_rows.push("<td>CORI</td><td style='text-align:right'>"+((cori_eur).toFixed(2))+"</td><td style='text-align:right' >"+((cori_val).toFixed(4))+"</td>");
 
             asset_rows.push("<td>Transaction Costs</td><td style='text-align:right'>"+(gasUsed*xrate_assets).toFixed(2)+"</td><td style='text-align:right'>"+((gasUsed*1).toFixed(4))+"</td>");
             if($('#balance').attr('data')!=null) {
+              asset_eths+=$('#balance').attr('data')*1;
+              asset_eurs+=$('#balance').attr('data')*1*xrate_assets;
               var accurals=liability_eths-asset_eths;
+              if(accurals>0) {
+                  asset_eths+=accurals;
+                  asset_eurs+=accurals*xrate_assets;
+                  asset_rows.push("<td>Accruals and Deferrals (SC)</td><td style='text-align:right'>"+((accurals*xrate_assets).toFixed(2))+"</td><td style='text-align:right' >"+((accurals).toFixed(4))+"</td>");
+              } else {
+                accurals=Math.abs(accurals);
+                liability_eths+=accurals;
+                liability_eurs+=accurals*xrate_assets;
+                liability_rows.push("<td>Accruals and Deferrals (SC)</td><td style='text-align:right'>"+((accurals*xrate_assets).toFixed(2))+"</td><td style='text-align:right' >"+((accurals).toFixed(4))+"</td>");
+              }
+              asset_rows.push("<th class='second'>BC Account</th><th style='text-align:right' class='second'>"+(($('#balance').attr('data')*xrate_assets).toFixed(2))+"</th><th style='text-align:right' class='second'>"+(($('#balance').attr('data')*1).toFixed(4))+"</th>");
 
-              console.log(asset_eths)
-              asset_eths+=accurals;
-              asset_eurs+=accurals*xrate_assets;
-              asset_rows.push("<td>Accurals and Deferrals (SC)</td><td style='text-align:right'>"+((accurals*xrate_assets).toFixed(2))+"</td><td style='text-align:right' >"+((accurals).toFixed(4))+"</td>");
-              liability_rows.push("<th class='second'>Equity BC Account</th><th style='text-align:right' class='second'>"+(($('#balance').attr('data')*xrate_assets).toFixed(2))+"</th><th style='text-align:right' class='second'>"+(($('#balance').attr('data')*1).toFixed(4))+"</th>");
             }
 
             if(asset_eurs>liability_eurs) {
@@ -208,7 +239,7 @@ function app(kyc) {
     });
     });
   }
-  getTransactionsClean();
+
 
 
 
